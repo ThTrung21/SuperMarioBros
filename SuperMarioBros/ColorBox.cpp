49ac2ddd516017cfd39347cbfb2acbd20338781c
zzbr21 <ntrung1121@gmail.com>
2023-06-10 20:43:23 +0700

    Add colorbox render logic

--- SuperMarioBros/ColorBox.cpp
+++ SuperMarioBros/ColorBox.cpp
@@ -36,6 +36,58 @@ void CColorBox::Render()
 {
 	if (this->Width <3 )	return;
 	if (this->Height < 3)	return;
+	if (Width < 3* cellWidth || Height < 3* cellHeight) return;
+	int sizeHor = Width / cellWidth; // numbers of cells horizontally
+	int sizeVer = Height / cellHeight;// number of cells vertically
+	//Declare reuseable start point
+	float xx = x;
+	float yy = y;
+
+	CSprites* s = CSprites::GetInstance();
+	// Draw Top Left
+	s->Get(spriteIDTopLeft)->Draw(x, y); 
+	//Draw Top Mid
+	xx += this->cellWidth;
+	for (int i = 0; i < sizeHor-2; i++) {
+		s->Get(spriteIDTopMid)->Draw(xx, yy);
+		xx += this->cellWidth;
+	}
+	//Draw Top Right
+	s->Get(spriteIDTopRight)->Draw(xx, yy);
+	//Draw Mid Right
+	yy += this->cellHeight;
+	for (int i = 0; i < sizeVer - 2; i++) {
+		s->Get(spriteIDMidRight)->Draw(xx, yy);
+		yy += this->cellHeight;
+	}
+	//Draw Bot Right
+	s->Get(spriteIDBotRight)->Draw(xx, yy);
+	//Draw Bot Mid
+	xx -= this->cellWidth;
+	for (int i = 0; i < sizeHor - 2; i++) {
+		s->Get(spriteIDBotMid)->Draw(xx, yy);
+		xx -= this->cellWidth;
+	}
+	//Draw Bot Left
+	s->Get(spriteIDBotLeft)->Draw(xx, yy);
+	//Draw Mid Left
+	yy -= this->cellHeight;
+	for (int i = 0; i < sizeVer - 2; i++) {
+		s->Get(spriteIDMidLeft)->Draw(xx, yy);
+		yy -= this->cellHeight;
+	}
+	//Draw Center
+	int xx_center = x + cellWidth;
+	for (int i = 0; i < sizeHor; i++) {
+		int yy_center = y + cellHeight;
+		for (int j = 0; j < sizeVer; j++) {
+			s->Get(spriteIDCenter)->Draw(xx, yy);
+			yy_center += cellHeight;
+		}
+		xx_center += cellWidth;
+	}
+
+	RenderBoundingBox();
 }
 
 void CColorBox::Update()
