e93018b2c28ff295bc68272aacb8f0e87a01e639
zzbr21 <ntrung1121@gmail.com>
2023-06-14 20:48:28 +0700

    add textures for fire plant

---
+++ SuperMarioBros/FirePlant.cpp
@@ -0,0 +1,29 @@
+#include "FirePlant.h"
+
+CFirePlant::CFirePlant(float x, float y) :CGameObject(x,y)
+{
+	this->vy = -1;
+
+}
+
+void CFirePlant::GetBoundingBox(float& left, float& top, float& right, float& bottom)
+{
+	left = x - PLANT_WIDTH / 2;
+	top = y - PLANT_HEIGHT / 2;
+	right = x + PLANT_WIDTH;
+	bottom = y + PLANT_HEIGHT;
+}
+
+void CFirePlant::OnNoCollision(DWORD dt)
+{
+	y += vy + dt;
+}
+
+void CFirePlant::OnCollisionWith(LPCOLLISIONEVENT e)
+{
+
+}
+void CFirePlant::SetState(int state)
+{	
+	CGameObject::SetState(state);
+}
--- SuperMarioBros/FirePlant.h
+++ SuperMarioBros/FirePlant.h
@@ -1,12 +1,33 @@
 #pragma once
 #include"GameObject.h"
+#define PLANT_WIDTH 16
+#define PLANT_HEIGHT 32
 
+#define PLANT_DIR_TOPLEFT	1
+#define PLANT_DIR_TOPRIGHT 2
+#define	PLANT_DIR_BOTTOMLEFT 3
+#define PLANT_DIR_BOTTOMRIGHT 4
 
 class CFirePlant :public CGameObject
 {
 protected:
 
+	ULONGLONG StopTime;
+	bool IsStop;
+	float TopMost;
+	float BotMost;
 
+
+	virtual void GetBoundingBox(float& left, float& top, float& right, float& bottom);
+	virtual void Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects);
+	virtual void Render();
+
+	virtual int IsCollidable() { return 1; };
+	virtual int IsBlocking() { return 0; }
+	virtual void OnNoCollision(DWORD dt);
+
+	virtual void OnCollisionWith(LPCOLLISIONEVENT e);
 public:
 	CFirePlant(float x, float y);
+	virtual void SetState(int state);
 };
