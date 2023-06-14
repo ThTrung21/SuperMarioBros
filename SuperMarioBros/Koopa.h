e568b06efc85b80df6da66125321fcf2740406be
zzbr21 <ntrung1121@gmail.com>
2023-06-15 00:10:09 +0700

    add CKoopa class

--- SuperMarioBros/Koopa.h
+++ SuperMarioBros/Koopa.h
@@ -14,4 +14,24 @@
 #define ID_ANI_KOOPA_RETRACT 16001
 #define ID_ANI_KOOPA_EXTEND 16002
 
-class 
+class CKoopa : public CGameObject
+{
+protected:
+	float ax;
+	float ay;
+
+	ULONGLONG shell_start;
+	virtual void GetBoundingBox(float& left, float& top, float& right, float& bottom);
+	virtual void Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects);
+	virtual void Render();
+
+	virtual int IsCollidable() { return 1; };
+	virtual int IsBlocking() { return 0; }
+	virtual void OnNoCollision(DWORD dt);
+
+	virtual void OnCollisionWith(LPCOLLISIONEVENT e);
+
+public:
+	CKoopa(float x, float y);
+	virtual void SetState(int state);
+};
